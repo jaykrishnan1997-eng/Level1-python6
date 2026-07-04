516ee0dd25cfe5edafa5b7fe1231cecf79a09500
Jay Krishnan <jkrishna@student.42heilbronn.de>
2026-07-04 10:07:41 +0200

    initial commit with folder and file structures

---
+++ alchemy/elements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   elements.py                                          :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: jkrishna <jkrishna@student.42.fr>            +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/07/04 10:05:16 by jkrishna            #+#    #+#            #
+#   Updated: 2026/07/04 10:05:59 by jkrishna           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+def create_earth() -> str:
+    return ("Earth element created")
+
+
+def create_air() -> str:
+    return ("Air element created")
---
+++ elements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   elements.py                                          :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: jkrishna <jkrishna@student.42.fr>            +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/07/04 10:03:27 by jkrishna            #+#    #+#            #
+#   Updated: 2026/07/04 10:05:06 by jkrishna           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+def create_fire() -> str:
+    return ("Fire element created")
+
+
+def create_water() -> str:
+    return ("Water element created")
